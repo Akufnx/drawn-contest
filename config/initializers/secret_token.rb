fae37275b6a4e25ce0147f040130f8f3bec17181
Louis Gilibert Mary <louis@printabit.com>
2015-04-25 15:51:07 +0200

    Initial Setup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || '5d143ecb1a7e241e0cbb69c9637cf8d4ada49743e6c321691a48c0fda36a046011260748b82b5142eea6be19e9e77a0198b57121802bb4494ef7bc48458722ae'
